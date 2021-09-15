296f3ee66331cc9a5831ab9f27716768464cf95b
unknown <hh4518@gmail.com>
2021-09-16 05:44:24 +0900

    eslint airbnb 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
-function App() {
-  return <div className="App"></div>;
+class App extends React.Component<any> {
+  render() {
+    return <div className="App"></div>;
+  }
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// router
+import { BrowserRouter } from 'react-router-dom';
+// app
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
