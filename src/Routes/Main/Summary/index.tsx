78938852281a0f3f30b75d4b7272f50bcbea988f
unknown <hh4518@gmail.com>
2021-09-17 07:54:08 +0900

    main page 설명 컴포넌트 완성

---
+++ src/Routes/Main/Summary/index.tsx
@@ -0,0 +1,81 @@
+import React from 'react';
+// icon
+import TextSnippetOutlinedIcon from '@mui/icons-material/TextSnippetOutlined';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import MovieCreationOutlinedIcon from '@mui/icons-material/MovieCreationOutlined';
+// elements
+import { Container } from '../../../elements';
+// style
+import './style.scss';
+
+const tabList = ['Text', 'To', 'Video'];
+
+const Summary = () => {
+  const [tab, setTab] = React.useState(0);
+
+  React.useEffect(() => {
+    const changeTab = setTimeout(
+      () => {
+        if (tab < 2) {
+          setTab((current) => current + 1);
+          return;
+        }
+
+        setTab(0);
+      },
+      tab === 1 ? 300 : 1000,
+    );
+
+    return () => {
+      clearTimeout(changeTab);
+    };
+  }, [tab]);
+
+  return (
+    <section className="section__summary">
+      <Container>
+        <div className="menu-wrapper">
+          <div className="menu-tab">
+            {tabList.map((list, idx) => (
+              <button
+                key={idx}
+                className="tab-list"
+                onClick={() => setTab(idx)}
+              >
+                {list}
+              </button>
+            ))}
+
+            <div
+              className="show-current-tab"
+              style={{ left: `${tab * 160}px` }}
+            />
+          </div>
+
+          <ul
+            className="slide-wrapper"
+            style={{ transform: `translateX(-${tab * 480}px)` }}
+          >
+            <li className="slide-list">
+              <TextSnippetOutlinedIcon style={{ fontSize: '200px' }} />
+            </li>
+
+            <li className="slide-list">
+              <ArrowForwardIcon style={{ fontSize: '100px' }} />
+            </li>
+
+            <li className="slide-list">
+              <MovieCreationOutlinedIcon style={{ fontSize: '200px' }} />
+            </li>
+          </ul>
+        </div>
+
+        <div className="vertical-divider" />
+
+        <h2 className="description">The easiest way to make videos</h2>
+      </Container>
+    </section>
+  );
+};
+
+export default Summary;
