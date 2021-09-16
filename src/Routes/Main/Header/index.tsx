243e9aafa2dc54a28fd6dc69e501628b50d59872
unknown <hh4518@gmail.com>
2021-09-17 07:53:23 +0900

    main page header 완성

---
+++ src/Routes/Main/Header/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+// router
+import { Link } from 'react-router-dom';
+// elements
+import { Button, Container } from '../../../elements';
+// style
+import './style.scss';
+
+const Header = () => {
+  return (
+    <header>
+      <Container>
+        <h1 className="logo">
+          <Link to="/">
+            <img
+              src="http://ttv.waynehills.co/assets/logo.png"
+              alt="waynehills ventures"
+              className="logo--img"
+            />
+          </Link>
+        </h1>
+
+        <Button color="black">
+          <Link to="/login">Sign in</Link>
+        </Button>
+      </Container>
+    </header>
+  );
+};
+
+export default Header;
