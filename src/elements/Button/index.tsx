6f74adf44f2a33f895bba27867f81f3b17842ce0
unknown <hh4518@gmail.com>
2021-09-17 05:47:19 +0900

    button elements 색상 선택 가능하도록 수정

--- src/elements/Button/index.tsx
+++ src/elements/Button/index.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import classNames from 'classnames';
+// style
+import './style.scss';
 
 type ButtonProps = {
+  onClick?: () => void;
   color: 'black' | 'white';
 };
 
-const Button: React.FC<ButtonProps> = ({ color, children }) => {
-  return <button className={classNames('btn', color)}>{children}</button>;
+const Button: React.FC<ButtonProps> = ({ onClick, color, children }) => {
+  return (
+    <button className={classNames('btn', color)} onClick={onClick}>
+      {children}
+    </button>
+  );
 };
 
 export default Button;
