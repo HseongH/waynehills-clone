0804edb3041ecf2166a6a7d69300074bf795e432
unknown <hh4518@gmail.com>
2021-09-17 15:51:24 +0900

    button elements 스타일 세분화

--- src/elements/Button/index.tsx
+++ src/elements/Button/index.tsx
@@ -5,12 +5,27 @@ import './style.scss';
 
 type ButtonProps = {
   onClick?: () => void;
-  color: 'black' | 'white';
+  color?: 'black' | 'white' | 'purple';
+  size?: 'middle' | 'large';
+  type?: 'button' | 'submit';
+  disabled?: boolean;
 };
 
-const Button: React.FC<ButtonProps> = ({ onClick, color, children }) => {
+const Button: React.FC<ButtonProps> = ({
+  onClick,
+  color,
+  size,
+  type,
+  disabled,
+  children,
+}) => {
   return (
-    <button className={classNames('btn', color)} onClick={onClick}>
+    <button
+      className={classNames('btn', color, size)}
+      type={type}
+      disabled={disabled}
+      onClick={onClick}
+    >
       {children}
     </button>
   );
