907ab44d810fa848db53fe06dc626c41b42d25e5
unknown <hh4518@gmail.com>
2021-09-17 12:39:21 +0900

    main page, sign in page route 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
+// router
+import { Switch, Route } from 'react-router-dom';
+// pages
+import Main from './Routes/Main';
+import SignIn from './Routes/SignIn';
 
 class App extends React.Component {
   render() {
-    return <div className="App"></div>;
+    return (
+      <div className="App">
+        <Switch>
+          <Route path="/" component={Main} exact />
+          <Route path="/login" component={SignIn} exact />
+        </Switch>
+      </div>
+    );
   }
 }
 
