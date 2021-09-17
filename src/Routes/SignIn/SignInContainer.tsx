948ca5821e10e3cdb5d378b5cd65e8261319cbc8
unknown <hh4518@gmail.com>
2021-09-17 15:54:48 +0900

    로그인 실패 시 경고 문구 출력 기능 추가

--- src/Routes/SignIn/SignInContainer.tsx
+++ src/Routes/SignIn/SignInContainer.tsx
@@ -2,9 +2,26 @@ import React from 'react';
 // present
 import SignInPresent from './SignInPresenter';
 
+type SignInState = {
+  loginStatus: 0 | 1 | 2;
+};
+
 class SignInContainer extends React.Component {
+  state: SignInState = {
+    loginStatus: 0,
+  };
+
+  handleSignIn = () => {
+    this.setState({ loginStatus: 2 });
+  };
+
   render() {
-    return <SignInPresent />;
+    return (
+      <SignInPresent
+        signInStatus={this.state.loginStatus}
+        signIn={this.handleSignIn}
+      />
+    );
   }
 }
 
--- src/Routes/SignIn/SignInPresenter.tsx
+++ src/Routes/SignIn/SignInPresenter.tsx
@@ -1,14 +1,47 @@
 import React from 'react';
+import classNames from 'classnames';
+// router
 import { Link } from 'react-router-dom';
 // elements
-import { Container } from '../../elements';
+import { Container, Button } from '../../elements';
 // style
 import './style.scss';
 
-const SignInPresenter: React.FC = (props) => {
+type SignInProps = {
+  signInStatus: number;
+  signIn: () => void;
+};
+
+const SignInPresenter: React.FC<SignInProps> = ({ signInStatus, signIn }) => {
+  const [visible, setVisible] = React.useState({
+    status: false,
+    message: '표시',
+  });
+
+  const handleSignIn = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    signIn();
+  };
+
+  const handleToggleVisible = () => {
+    const { status } = visible;
+
+    setVisible({
+      ...visible,
+      status: !status,
+      message: status ? '표시' : '숨기기',
+    });
+  };
+
   return (
     <main className="signin">
       <section className="section__signin">
+        {signInStatus === 2 ? (
+          <div className="alert">
+            입력하신 이메일과 비밀번호가 일치하지 않습니다.
+          </div>
+        ) : null}
+
         <h1 className="signin-logo">
           <img
             src="http://ttv.waynehills.co/assets/i_logo_w_nomal.png"
@@ -28,26 +61,44 @@ const SignInPresenter: React.FC = (props) => {
           <div className="signin-inner">
             <h2 className="title">로그인</h2>
 
-            <form id="signin-form">
-              <input
-                className="input--text"
-                type="email"
-                placeholder="이메일 입력"
-              />
-              <input
-                className="input--text"
-                type="password"
-                placeholder="비밀번호 입력"
-              />
+            <form id="signin-form" onSubmit={handleSignIn}>
+              <div
+                className={classNames('enter-area', {
+                  failed: signInStatus === 2,
+                })}
+              >
+                <input
+                  className={classNames('input--text')}
+                  type="email"
+                  placeholder="이메일 입력"
+                />
+              </div>
+
+              <div
+                className={classNames('enter-area', {
+                  failed: signInStatus === 2,
+                })}
+              >
+                <input
+                  className="input--text"
+                  type={visible.status ? 'text' : 'password'}
+                  placeholder="비밀번호 입력"
+                />
+
+                <Button type="button" onClick={handleToggleVisible}>
+                  {visible.message}
+                </Button>
+              </div>
 
               <div className="forgot-pwd">
                 <p>비밀번호를 잊으셨나요?</p>
+
                 <Link to="login">비밀번호 찾기</Link>
               </div>
 
-              <button className="btn btn--signin" type="submit">
+              <Button size="large" color="purple" type="submit">
                 로그인
-              </button>
+              </Button>
             </form>
 
             <div className="save-signin-info">
@@ -81,9 +132,10 @@ const SignInPresenter: React.FC = (props) => {
 
             <div className="link-to-signup-page">
               <p>회원가입하고 영상을 제작해보세요!</p>
-              <button className="btn btn--signup" disabled>
+
+              <Button size="large" color="black" disabled>
                 회원가입
-              </button>
+              </Button>
             </div>
           </div>
         </div>
